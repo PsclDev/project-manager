3a00128ef23182355c0e7c53ba472fb21be388ba
Pascal <github@unrundead.eu>
2019-03-28 14:33:08 +0100

    Created Objects: Meeting, Note, ToDo inherit from Property

---
+++ ProjectManager/Objects/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager
+{
+    public class Note : Property
+    {
+        public Note(int id, string text) : base(id, text)
+        {
+
+        }
+    }
+}
---
+++ ProjectManager/Objects/ToDo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager
+{
+    public class ToDo : Property
+    {
+        public ToDo(int id, string text) : base(id, text)
+        {
+
+        }
+    }
+}
